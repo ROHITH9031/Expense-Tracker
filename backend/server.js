d4fe9eadbf147defa296b10e21607e8e6eaa4e47
AtyamRohith <atyamrohith2005@gmail.com>
2026-09-06 11:11:07 +0530

    Allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(
 cors({
-origin: ["http://localhost:5173", "http://localhost:5174"],
+origin: ["http://localhost:5173", "https://expense-tracker-delta-opal-20.vercel.app/"],
 credentials: true,
 })
 );
