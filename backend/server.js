d6941d492477b160405dbe1b018e9f77430c7088
AtyamRohith <atyamrohith2005@gmail.com>
2026-09-06 11:27:06 +0530

    Resolved the production issues

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,15 @@ const app = express();
 
 // Middleware
 app.use(
-cors({
-origin: ["http://localhost:5173", "https://expense-tracker-delta-opal-20.vercel.app/"],
-credentials: true,
-})
+  cors({
+    origin: [
+      "http://localhost:5173",
+        "http://localhost:5174",
+
+      "https://expense-tracker-delta-opal-20.vercel.app",
+    ],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
@@ -28,10 +33,10 @@ connectDB();
 
 // Test Route
 app.get("/", (req, res) => {
-res.json({
-success: true,
-message: "Expense Tracker API is running",
-});
+  res.json({
+    success: true,
+    message: "Expense Tracker API is running",
+  });
 });
 
 // Routes
@@ -42,14 +47,14 @@ app.use("/api/reports", reportRoutes);
 
 // Handle unknown routes
 app.use((req, res) => {
-res.status(404).json({
-success: false,
-message: "Route not found",
-});
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
