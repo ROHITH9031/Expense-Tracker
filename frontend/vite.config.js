1e2e81c7a0547b956d24ad28f9a0f1e59465135b
AtyamRohith <atyamrohith2005@gmail.com>
2026-09-13 11:18:11 +0530

    Made some changes to get an APK app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,36 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Expense Tracker",
+        short_name: "Expenses",
+        description: "Manage your income and expenses easily",
+        theme_color: "#0f172a",
+        background_color: "#0f172a",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
